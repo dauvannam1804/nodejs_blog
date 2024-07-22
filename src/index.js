c928c155f03d752127c02a31490d47a03b187bc1
dauvannam1804 <dauvannam321@gmail.com>
2024-07-22 20:35:12 +0700

    format code

--- src/index.js
+++ src/index.js
@@ -1,34 +1,39 @@
-const path = require("path")
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const port = 3000;
 const handlebars = require('express-handlebars');
-const exp = require("constants");
+const exp = require('constants');
 
-const route = require('./routes')
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
 app.use(express.json());
 
 // HTTP logger
 // app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', handlebars.engine({
-  extname:".hbs"
-}));
+app.engine(
+  'hbs',
+  handlebars.engine({
+    extname: '.hbs',
+  }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
-console.log(__dirname)
+console.log(__dirname);
 
 // Routes Init
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
