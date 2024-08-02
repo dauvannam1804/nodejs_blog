3c20ef8b5590b31fd6c9a510f5187280c7765d9d
dauvannam1804 <dauvannam321@gmail.com>
2024-08-02 22:50:01 +0700

    add: update courses

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -11,6 +11,41 @@ class CourseController {
             .catch(next);
         // res.send('COURSE DETAIL - ' + req.params.slug);
     }
+
+    // [GET] /courses/create
+    create(req, res, next) {
+        res.render('courses/create');
+    }
+
+    // [POST] /courses/store
+    store(req, res, next) {
+        // res.json(req.body);
+        const formData = req.body;
+        formData.image = `https://img.youtube.com/vi/${req.body.videoID}/sddefault.jpg`;
+        const course = new Course(req.body);
+        course.save();
+        res.send("COURSE SAVED");
+            // .then(() => res.direct('/'))
+            // .catch(error => {
+
+            // });
+    }
+
+    // [GET] /courses/:id/edit
+    edit(req, res, next) {
+        Course.findById(req.params.id)
+            .then(course => res.render('courses/edit',{
+                course: mongooseToObject(course)
+            }))
+            .catch(next)
+    }
+
+    // [PUT] /courses/:id
+    update(req, res, next){
+        Course.updateOne({_id: req.params.id}, req.body)
+            .then(() => res.redirect('/me/stored/courses'))
+            .catch(next);
+    }
 }
 
 module.exports = new CourseController();
